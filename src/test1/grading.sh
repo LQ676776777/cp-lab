c1de2d412824f2219ef1714d89b888907d41c8f4
杨茂林 <8575543@qq.com>
2021-11-19 01:04:44 +0800

    Edit by browser

--- src/test1/grading.sh
+++ src/test1/grading.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
 #make clean（清除上一次的评测程序）
-rm pl_test lex.yy.c > /dev/null 2>&1  
+rm pl0.l lex.yy.c > /dev/null 2>&1  
 
 #新建文本，准备存放程序输出结果
 touch tokens.txt
@@ -12,7 +12,7 @@ touch error.txt
 #读取键盘输入到typ中         
 read -p '' typ	
 	         
-flex pl_test.l > error.txt 2>&1	         
+flex pl0.l > error.txt 2>&1	         
 #用flex编译源文件，保存错误信息
 
 #检查是否成功生成
@@ -22,9 +22,9 @@ echo "下面是错误信息："
 cat error.txt
 else
 #生成最终的可执行程序文件
-gcc -o pl_test lex.yy.c         
+gcc -o pl0 lex.yy.c         
 #执行最终生成的程序
-./pl_test test_cases/pl/Test2_$typ.PLS > tokens.txt    
+./pl0 test_cases/pl/case_$typ.pl0 > tokens.txt    
 
 #显示程序运行结果
 cat tokens.txt											

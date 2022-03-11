90af8778647fa6dff676f7c1c7596fc7044978ce
杨茂林 <8575543@qq.com>
2022-03-11 22:10:11 +0800

    Edit by browser

--- src/alibab.c
+++ src/alibab.c
@@ -1,1 +1,16 @@
- 1
+#include <stdio.h>
+#include <alibaba.h>
+ 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+ 
+void alibaba(void)
+{
+   int y;
+   printf("My name is Alibaba, I am %d years old.\n", y);
+ 
+   #ifdef BILIBILI
+   printf("Alibaba: Hey Bili, お久しぶり.\n");
+   #endif
+}

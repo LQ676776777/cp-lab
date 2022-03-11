5c8c60757dcf813fd2eab571d0c56aeeb14d321e
杨茂林 <8575543@qq.com>
2022-03-11 21:18:05 +0800

    Edit by browser

--- src/gcd.c
+++ src/gcd.c
@@ -1,1 +1,19 @@
- 
+#include <stdio.h>
+
+int main()
+{
+    int m, n;
+	        
+    printf("Enter two positive integers: ");
+    scanf("%d %d",&m, &n);
+
+    while(m != n) {
+       if(m > n)  m -= n;
+       else n -= m;
+       
+     }
+
+    printf("GCD = %d\n",m);
+    
+    return 0;
+}  

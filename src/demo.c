59893181cb15aed076f42b3f686a7c1b41538b80
杨茂林 <8575543@qq.com>
2022-03-18 21:28:11 +0800

    demo.c

---
+++ src/demo.c
@@ -0,0 +1,14 @@
+ int foo(int a, int b){
+    return a + b + 10;
+}
+
+int  main(){
+   int i = 10, j = 20;
+   int k;
+
+    k = foo(i,j);
+   putint(k);
+   putch(13);
+   putch(10);
+    return 0;
+}

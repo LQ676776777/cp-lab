019f98656f5b86ca3205af804a47c1c855e6d9db
杨茂林 <8575543@qq.com>
2022-04-24 13:27:09 +0800

    Edit by browser

--- grading1.sh
+++ grading1.sh
@@ -1,3 +1,7 @@
 chmod +x src/shell1.sh
-.src/sehll1.sh
-if []
+.src/shell1.sh > err1.txt
+if [ ! -f "src/def-test" ];then	         
+echo "def-test，编译未成功，请检查编译脚本。"
+echo "下面是错误信息："
+cat err1.txt
+else

58c745aa12536e84687aecad1b727206619b86f9
杨茂林 <8575543@qq.com>
2022-04-24 21:20:57 +0800

    Edit by browser

--- src/shell3.sh
+++ src/shell3.sh
@@ -1,5 +1,8 @@
  #!/bin/bash
 
-# 请用 用arm-linux-gnueabihf-gcc交叉编译器把bar.c“翻译”成优化的(优化级别O2) armv7架构，linux系统，符合gnueabihf嵌入式二进制接口规则，
-# 并支持arm硬浮点的汇编代码（程序中并没有浮点数）。汇编代码文件名为bar.clang.arm.s
-# 请使用恰当的编译选项以完成上述任务：
+# 用arm-linux-gnueabihf-gcc 将iplusf.c编译成arm汇编代码iplusf.arm.s
+
+# 再次用arm-linux-gnueabihf-gcc 汇编iplusf.arm.s，同时连接SysY2022的运行时库sylib.a，生成arm的可执行代码iplusf.arm
+
+# 用qemu-arm运行iplusf.arm
+

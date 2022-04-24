c87d1d721509bc4ad6ee70ffd289a39d3c93122a
杨茂林 <8575543@qq.com>
2022-04-24 15:26:55 +0800

    Edit by browser

--- grading2.sh
+++ grading2.sh
@@ -1,10 +1,10 @@
 cd src
-chmod +x shell1.sh
-./shell1.sh > err1.txt
-if [ ! -f "def-test" ];then	         
-echo "未找到def-test，编译未成功，请检查编译脚本。"
+chmod +x shell2.sh
+./shell2.sh > err2.txt
+if [ ! -f "bar.clang.s" ];then	         
+echo "未找到bar.clang.s，编译未成功，请检查编译脚本。"
 echo "下面是错误信息："
-cat err1.txt
+cat err2.txt
 else
-./def-test 
+cat bar.clang.s
 fi 

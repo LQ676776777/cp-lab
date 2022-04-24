867b82f7fdb99d8ac26ceae2b3b1e8e0031eb8cf
杨茂林 <8575543@qq.com>
2022-04-24 16:18:11 +0800

    Edit by browser

--- src/shell2.sh
+++ src/shell2.sh
@@ -1,4 +1,7 @@
  #!/bin/bash
 
-# 请用 gcc 编译器，连编def-test.c, alibaba.c，生成def-test二进制可执行代码。通过在编译命令行加适当的参数，
-# 使生成的程序能形成Alibaba和Bili两个人的对话：
+# 请用 用clang编译器把bar.c“翻译”成优化的(优化级别O2)armv7架构，linux系统，符合gnueabihf嵌入式二进制接口规则，
+# 并支持arm硬浮点的汇编代码（程序中并没有浮点数）。汇编代码文件名为bar.clang.arm.s
+# 请使用恰当的编译选项以完成上述任务：
+
+

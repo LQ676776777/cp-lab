0334167a1d73fb3d2ee14053788a7639e065f8f9
杨茂林 <8575543@qq.com>
2022-04-24 15:32:32 +0800

    Edit by browser

--- grading2.sh
+++ grading2.sh
@@ -1,10 +1,10 @@
 cd src
 chmod +x shell2.sh
 ./shell2.sh > err2.txt
-if [ ! -f "bar.clang.s" ];then	         
-echo "未找到bar.clang.s，编译未成功，请检查编译脚本。"
+if [ ! -f "bar.clang.arm.s" ];then	         
+echo "未找到bar.clang.arm.s，编译未成功，请检查编译脚本。"
 echo "下面是错误信息："
 cat err2.txt
 else
-cat bar.clang.s
+cat bar.clang.arm.s
 fi 

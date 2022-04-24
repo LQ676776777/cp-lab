e3f7d32c9bc159ddb3fd89ba3fe7003122a487b4
杨茂林 <8575543@qq.com>
2022-04-24 21:36:23 +0800

    Edit by browser

--- grading3.sh
+++ grading3.sh
@@ -3,8 +3,8 @@ chmod +x shell3.sh
 
 if [ $(cat shell3.sh  | grep -c "arm-linux-gnueabihf-gcc") -eq 0 ]; then 
 echo 'arm-linux-gnueabihf-gcc not used!' 
-elif  $(cat shell3.sh  | grep -c "qemu-arm") -eq 0 ]; then 
+elif [ $(cat shell3.sh  | grep -c "qemu-arm") -eq 0 ]; then 
 echo 'qemu-arm not used!' 
 else
-    ./shell3.sh
+./shell3.sh
 fi

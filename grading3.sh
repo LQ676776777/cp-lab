44cdc55114a283cc74e9fbd0c9ea300913bd8d22
杨茂林 <8575543@qq.com>
2022-04-24 21:17:43 +0800

    Edit by browser

--- grading3.sh
+++ grading3.sh
@@ -1,11 +1,10 @@
 cd src
 chmod +x shell3.sh
-./shell3.sh 
 
-if [ ! -f "bar.armgcc.arm.s" ];then	         
-echo "未找到bar.clang.arm.s，编译未成功，请检查编译命令及编译选项。"
-echo "下面是错误信息："
-
-else
-cat bar.clang.arm.s
-fi  
+if [ $(cat shell3.sh  | grep -c "arm-linux-gnueabihf-gcc") -eq 0 ]; then 
+echo 'arm-linux-gnueabihf-gcc not used!' 
+elif  $(cat sehell3.sh  | grep -c "qemu-arm") -eq 0 ]; then 
+echo 'qemu-arm not used' 
+elif 
+    ./shell3.sh
+fi

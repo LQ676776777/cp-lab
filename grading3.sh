6a7dd28bbdbc9b92548d0ead3f65dd5cf8fcadd0
杨茂林 <8575543@qq.com>
2022-04-24 20:59:52 +0800

    Edit by browser

--- grading3.sh
+++ grading3.sh
@@ -1,6 +1,7 @@
 cd src
 chmod +x shell3.sh
-./shell3.sh > /dev/null 2>&1 
+./shell3.sh 
+
 if [ ! -f "bar.armgcc.arm.s" ];then	         
 echo "未找到bar.clang.arm.s，编译未成功，请检查编译命令及编译选项。"
 echo "下面是错误信息："

11d69f9e99bf0b94257696f96936c1ba0eba652d
杨茂林 <8575543@qq.com>
2022-04-24 13:33:55 +0800

    Edit by browser

--- grading1.sh
+++ grading1.sh
@@ -1,7 +1,7 @@
 chmod +x src/shell1.sh
 .src/shell1.sh > err1.txt
 if [ ! -f "src/def-test" ];then	         
-echo "def-test，编译未成功，请检查编译脚本。"
+echo "未找到def-test，编译未成功，请检查编译脚本。"
 echo "下面是错误信息："
 cat err1.txt
 else
